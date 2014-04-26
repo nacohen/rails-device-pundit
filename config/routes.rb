c64f6713e6cc59896c8f28afcd0f6e8702768296
Norm Cohen <nacohen@mac.com>
2014-04-25 19:48:51 -0700

    Made a contact page with simple form

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ RailsDevicePundit::Application.routes.draw do
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
+  resources :contacts, only: [:new, :create]
 end
---
+++ spec/features/visitor/contact_form_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+# Feature: Contact Page
+#   As a visitor
+#   I want to visit a Contact page to fill out a form with my name, email address and some text
+#   So I can send a message to the owner of the Web site
+feature 'Contact:' do
+# Scenario: Visit the Contact Page
+#   Given I am a visitor
+#   When I visit the Contact page
+#   Then I should see the Form
+  describe "form" do
+    subject {page}
+
+    before { visit new_contact_path }
+
+    it { should have_field("contact[name]") }
+    it { should have_field("contact[email]") }
+    it { should have_field("contact[content]") }
+  end
+end
+
+
